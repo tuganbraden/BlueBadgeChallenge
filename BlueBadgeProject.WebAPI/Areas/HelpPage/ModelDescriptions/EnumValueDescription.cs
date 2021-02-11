2f422c270274de6c246ac3b1368e7f1a62e1b7dc
tuganbraden <tuganbraden@gmail.com>
2021-02-11 17:50:19 -0500

    Add project files.

---
+++ BlueBadgeProject.WebAPI/Areas/HelpPage/ModelDescriptions/EnumValueDescription.cs
@@ -0,0 +1,11 @@
+namespace BlueBadgeProject.WebAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class EnumValueDescription
+    {
+        public string Documentation { get; set; }
+
+        public string Name { get; set; }
+
+        public string Value { get; set; }
+    }
+}
