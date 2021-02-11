2ddf2c32fec5cba58b28595cfadbc7527bad3e32
tuganbraden <tuganbraden@gmail.com>
2021-02-11 18:03:35 -0500

    Initial Commit

--- BlueBadgeProject.Data/IdentityModels.cs
+++ BlueBadgeProject.Data/IdentityModels.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
 
-namespace BlueBadgeProject.WebAPI.Models
+namespace BlueBadgeProject.Data
 {
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
