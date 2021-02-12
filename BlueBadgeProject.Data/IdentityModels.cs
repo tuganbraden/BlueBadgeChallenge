402a641175f21112adec12421e833043faf71e5a
Connor Braeger <cjbraeger@gmail.com>
2021-02-11 20:38:25 -0500

    Separated Clients from IdentityModels.cs for easier readability

--- BlueBadgeProject.Data/IdentityModels.cs
+++ BlueBadgeProject.Data/IdentityModels.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.ComponentModel.DataAnnotations;
-using System.Data.Entity;
+﻿using System.Data.Entity;
 using System.Data.Entity.ModelConfiguration;
 using System.Data.Entity.ModelConfiguration.Conventions;
-using System.Security.Claims;
-using System.Threading.Tasks;
-using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
 
@@ -23,51 +18,6 @@ namespace BlueBadgeProject.Data
     public enum SubscriberStatus
     {
         Free, Premium, GoldTier
-    }
-    public class Client : IdentityUser
-    {
-        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<Client> manager, string authenticationType)
-        {
-            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
-            var userIdentity = await manager.CreateIdentityAsync(this, authenticationType);
-
-            // Add custom user claims here
-
-            return userIdentity;
-        }
-        public string ClientId { get { return this.Id; } set { this.ClientId = this.Id; } }// primary key is inherited 
-        [Required]
-
-        public string FullName { get; set; }
-        [Required]
-        public DateTimeOffset CreatedUtc { get; set; }
-        [Required]
-
-        public double Height { get; set; }
-        
-        [Required]
-        public double Weight { get; set; }
-        [Required]
-        public double GoalWeight { get; set; }
-        [Required]
-        public DateTime GoalDate { get; set; }
-        [Required]
-        public SubscriberStatus SubscriberStatus { get; set; } = 0;
-        [Required]
-        public double WeeklyCaloricNeed { get; set; }
-        public BodyType BodyType { get; set; }
-        [Required]
-        public LifeStyleType LifeStyleType { get; set; }
-        [Required]
-        public bool IsVegetarian { get; set; } = false;
-        [Required]
-        public bool IsKeto { get; set; } = false;
-        [Required]
-        public bool IsLactoseFree{ get; set; } = false;
-        [Required]
-        public bool IsGlutenFree { get; set; } = false;
-
-
     }
 
     public class ApplicationDbContext : IdentityDbContext<Client>
