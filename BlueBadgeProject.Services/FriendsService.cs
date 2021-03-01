d9686a1e725b1e07fa8349196c62bc104e2bc9d3
anicodesit <ani.kulka@gmail.com>
2021-02-28 21:57:00 -0500

    Created Friends Data, Models, Service, Controller. Still working on an endpoint to view friends WorkOutPlan

---
+++ BlueBadgeProject.Services/FriendsService.cs
@@ -0,0 +1,66 @@
+﻿using BlueBadgeProject.Data;
+using BlueBadgeProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueBadgeProject.Services
+{
+    public class FriendsService
+    {
+        //Add Friend
+        public bool AddFriends(FriendsCreate model)
+
+        {
+            var entity =
+                new Friends()
+                {
+                    FriendUserId = model.FriendUserId,
+                    MyUserId = model.MyUserId
+                };
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Friends.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+
+        }
+        //TODO - go through a 2nd lambda to add the friendd name to the list here
+        public List<FriendsListItem> GetListOfFriends(string Id)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                    .Friends
+                    .Where(e => Id == e.MyUserId)
+                    .Select(e =>
+                        new FriendsListItem
+                        {
+                            MyUserId = e.MyUserId,
+                            FriendUserId = e.FriendUserId
+                        }
+                        );
+                return query.ToList();
+            }
+        }
+         //Delete Friend
+        public bool DeleteFriend(FriendsDetail model)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                    .Friends
+                    .Single(e => e.FriendUserId == model.FriendUserId && e.MyUserId == model.MyUserId);
+                ctx.Friends.Remove(entity);
+
+                return ctx.SaveChanges() == 1;
+            }
+        }
+        //View WorkoutPlan by Friend I
+    }
+}
+
---
+++ BlueBadgeProject.WebAPI/Controllers/FriendsController.cs
@@ -0,0 +1,66 @@
+﻿using BlueBadgeProject.Models;
+using BlueBadgeProject.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.ModelBinding;
+
+namespace BlueBadgeProject.WebAPI.Controllers
+{
+    [RoutePrefix("api/Friends")]
+    public class FriendsController: ApiController
+    {       
+        FriendsService friendsService = new FriendsService();
+        [HttpPost]
+        public IHttpActionResult AddYourFriends([FromBody] FriendsCreate model)
+        {
+            if (model is null)
+            {
+                return BadRequest("Your request body cannot be empty. ");
+            }
+            if (ModelState.IsValid)
+            {
+
+                var friendsAdded = friendsService.AddFriends(model);
+                if (friendsAdded == true)
+                {
+                    return Ok();
+                }
+                else
+                {
+                    return BadRequest();
+                }
+
+            }
+            return BadRequest(ModelState);
+        }
+        [HttpGet]
+        public IHttpActionResult GetFriendsList([FromBody] string Id)
+        {
+            return (IHttpActionResult)friendsService.GetListOfFriends(Id);
+
+        }
+        [HttpDelete]
+        public IHttpActionResult FriendsDelete(FriendsDetail model)
+        {
+            if (model is null)
+            {
+                return BadRequest();
+            }
+            if (ModelState.IsValid)
+            {
+                var friendIsDeleted = friendsService.DeleteFriend(model);
+                if (friendIsDeleted == true)
+                    return Ok();
+                else
+                    return BadRequest();
+            }
+
+            return BadRequest(ModelState);
+        }
+
+    }
+
+}
