c24cfd2393913f0c175294bea6c6b08a88a41b2e
Connor Braeger <cjbraeger@gmail.com>
2021-03-06 16:19:51 -0500

    Added ui for users

--- BlueBadgeProject.GUI/App_Start/RouteConfig.cs
+++ BlueBadgeProject.GUI/App_Start/RouteConfig.cs
@@ -14,9 +14,12 @@ namespace BlueBadgeProject.GUI
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                namespaces:    new []{ "BlueBadgeProject.GUI.Controllers"}
+
             );
         }
     }
